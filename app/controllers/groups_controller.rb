4c8ac0dc45c5e56e3b7217c3a30fd023946435d6
Egor Dovnar <egordovnar.job@gmail.com>
2018-12-08 14:46:45 +0300

    Implemented group adding (#new and #create methods). Added a method to instantly set current_user as an adult for new group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,14 +3,17 @@ class GroupsController < ApplicationController
     @groups = current_user.groups
   end
 
+  # POST /groups
   def create
     @group = Group.new(group_params)
 
     if @group.save
+      add_current_user_as_adult(@group)
       redirect_to @group
     end
   end
 
+  # GET /groups/new
   def new
     @group = Group.new
   end
@@ -19,8 +22,10 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  # GET groups/:id
   def show
     @group = Group.find(params[:id])
+    redirect_to root_path unless check_priviledge
   end
 
   def update
@@ -39,6 +44,20 @@ class GroupsController < ApplicationController
   end
 
   private
+
+  def check_priviledge
+    current_user.groups.include?(Group.find(params[:id])) &&
+        current_user.memberships.find_by(group_id: params[:id]).role == "adult"
+  end
+
+  def add_current_user_as_adult(group)
+    membership = Membership.create(
+      group_id: group.id,
+      user_id: current_user.id,
+      role: :adult
+    )
+    membership.save
+  end
 
   def group_params
     params.require(:group).permit(:name, :password, :visible_to_all)
