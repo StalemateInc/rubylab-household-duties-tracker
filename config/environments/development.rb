b34acae905a6360d38d5542809134eae32906cbd
Egor Dovnar <egordovnar.job@gmail.com>
2019-01-08 13:02:27 +0300

    Updated mailer configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,9 +38,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.default_options = { from: 'householdduties@example.com' }
   config.action_mailer.mailgun_settings = {
     domain: Rails.application.credentials.mailgun[:domain],
-    api_key: Rails.application.credentials.mailgun[:private_api_key],
+    api_key: Rails.application.credentials.mailgun[:private_api_key]
   }
 
   # Print deprecation notices to the Rails logger.
